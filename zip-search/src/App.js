1c4ad95a1a71dca020794b742e10e1c704acb523
KanG98 <kylexue@Yankangs-MBP.fios-router.home>
2022-01-18 14:48:07 -0500

    fixed data state

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -4,8 +4,9 @@ import NavBar from "./components/NavBar";
 import SearchBar from "./components/SearchBar";
 import queryZipcode from "./script/queryZipcode";
 
+
 function App() {
-  const [data, setData] = useState(sampleData) // need to be linkd to queryZipcode function
+  const [data, setData] = useState([]) // need to be linkd to queryZipcode function
   const [curSearch, setCurSearch] = useState("10001")
 
 
